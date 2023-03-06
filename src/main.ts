19f6c6c603b9cbb1eb0544a7ef8343aa01ed1fd1
陈文 <1204456064@qq.com>
2023-03-06 18:27:47 +0800

    feat: 引入动态粒子组件particles.vue3以及sass

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 import router from './router'
 import 'ant-design-vue/dist/antd.css'
+import Particles from 'particles.vue3'
 
 
 const app = createApp(App)
 app.use(router)
 app.use(Antd)
+app.use(Particles)
 
 
 app.mount('#app')
